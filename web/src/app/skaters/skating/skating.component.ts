4bbe0007ae91fd773f359805ac1a1bd1a17a4d00
Anthony Burch <burch.anthony@mayo.edu>
2023-12-04 19:05:16 -0600

    Work in progress

--- web/src/app/skaters/skating/skating.component.ts
+++ web/src/app/skaters/skating/skating.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+interface SkatingTableElement {
+  player: string,
+  team: string,
+  position: string,
+  topSpeed: number,
+  twentyPlus: number,
+  twentyTwentyTwo: number,
+  eighteenTwenty: number
+}
 
 @Component({
   selector: 'app-skating',
   templateUrl: './skating.component.html',
   styleUrls: ['./skating.component.scss']
 })
-export class SkatingComponent {
+export class SkatingComponent implements OnInit {
+  firstNames = ['John', 'Josh', 'Jacob', 'Jack', 'Jason'];
+  skatingTableElements: SkatingTableElement[] = [];
 
+  ngOnInit(): void {
+    this.skatingTableElements.push({
+      player: this.firstNames[Math.floor(Math.random()*this.firstNames.length)],
+      team: 'MIN',
+      position: 'D', 
+      topSpeed: 50,
+      twentyPlus: 6,
+      twentyTwentyTwo: 7,
+      eighteenTwenty: 38
+    });
+  }
 }
